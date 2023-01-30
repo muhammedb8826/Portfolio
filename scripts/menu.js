b5dccbd9ed07872c9f07933bafd84852751b3d82
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-01-30 15:57:46 -0600

    Add menu.js + event listeners

---
+++ scripts/menu.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById('menuBtn');
+const nav = document.querySelector('nav');
+const menuLink = document.getElementsByClassName('menu-link');
+
+menuBtn.onclick = ()=> {
+    nav.classList.toggle('active');
+}
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
