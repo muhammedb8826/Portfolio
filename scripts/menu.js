a2a7dc640133da497a9461ea63cb61691a0bba65
Muhammed Berisso <muhammedb8826@gmail.com>
2023-01-31 06:12:54 +0300

    Fix linter eslint error and event

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,11 @@
-const menuBtn = document.getElementById('menuBtn');
 const nav = document.querySelector('nav');
 const menuLink = document.getElementsByClassName('menu-link');
-
-menuBtn.onclick = ()=> {
+const menuBtn = document.getElementById('menuBtn');
+menuBtn.onclick = () => {
+  nav.classList.toggle('active');
+};
+for (let i = 0; i < menuLink.length; i += 1) {
+  menuLink[i].addEventListener('click', () => {
     nav.classList.toggle('active');
-}
-
-for (let i = 0; i < menuLink.length; i++) {
-    menuLink[i].addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
+  });
 }
