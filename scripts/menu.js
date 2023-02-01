789221185945b109d8af857783dbad5286399298
Muhammed Berisso <muhammedb8826@gmail.com>
2023-02-01 21:14:02 +0300

    Fix: Toggle menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,8 +9,8 @@ menuBtn.onclick = () => {
 };
 for (let i = 0; i < menuLink.length; i += 1) {
   menuLink[i].addEventListener('click', () => {
-    nav.classList.remove('active');
-    menuBtn.classList.remove('change');
-    menuBg.classList.remove('change-bg');
+    nav.classList.toggle('active');
+    menuBtn.classList.toggle('change');
+    menuBg.classList.toggle('change-bg');
   });
 }
