bd9f80dd69105deaf5f967d82af15da0465eb8ef
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-03 17:07:13 -0600

    Add localStorage.js

---
+++ scripts/localStorage.js
@@ -0,0 +1,7 @@
+const lSname = document.getElementById('your-name');
+const lSemail = document.getElementById('email');
+const lSmssage = document.getElementById('form-text-area');
+const inputs = Array.from(document.getElementsByClassName('formInput'));
+
+inputs.forEach((input) => input.addEventListener('input', () => {
+  }))
